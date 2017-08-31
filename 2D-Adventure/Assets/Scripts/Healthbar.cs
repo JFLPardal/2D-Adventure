e62c5b0e848c81cb6c7084475d09272987d9e84c
JFLPardal <jflpardal@gmail.com>
2017-08-31 15:46:44 +0100

    UI
    * added UI with healthbar

---
+++ 2D-Adventure/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour {
+
+	private RectTransform transform;
+
+	public float maxSpeed = 10f;
+
+	void Awake () 
+	{
+		transform = GetComponent<RectTransform> ();
+	}
+
+	void Start()
+	{
+		if (PlayerController.instance != null)
+			transform.sizeDelta = new Vector2 (Mathf.Clamp (PlayerController.health, 0, 100),
+											  transform.sizeDelta.y);
+	}
+
+	void Update () 
+	{
+		float healthUpdate = 0f;
+
+		if (PlayerController.instance != null) 
+			healthUpdate = Mathf.MoveTowards (transform.rect.width, PlayerController.health, maxSpeed);
+
+		transform.sizeDelta = new Vector2 (Mathf.Clamp (healthUpdate, 0, 100),
+										  transform.sizeDelta.y);
+	}
+}
